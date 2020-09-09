5e64d427b48d356a8e3bbf4e0eaed8ba6da07a35
UnrealMay <47468727+UnrealMay@users.noreply.github.com>
2020-09-09 22:34:48 +0800

    Update 17wanxiao.py

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -36,9 +36,9 @@ jsons = {
 }
 
 nowtime = datetime.datetime.now()
-if nowtime.hour > 22
+if nowtime.hour > 22:
 	jsons["customerAppTypeRuleId"] = 146
-if nowtime.hour > 13
+if nowtime.hour > 13:
 	jsons["customerAppTypeRuleId"] = 148
 
 response = requests.post(check_url, json=jsons)
